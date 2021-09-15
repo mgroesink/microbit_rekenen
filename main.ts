e9f03f8fd3c890d9453010d50bfdc7e856c78b05
Marcel Roesink <mroesink@rocvantwente.nl>
2021-09-15 10:16:28 +0200

    Knop B toegevoegd

--- main.ts
+++ main.ts
@@ -2,11 +2,21 @@ input.onButtonPressed(Button.A, function () {
     totaal = 0
     for (let waarde of list) {
         totaal += waarde
+        basic.showString("Totaal: " + totaal)
     }
-    basic.showNumber(totaal)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let waarde of list) {
+        if (waarde > hoogste) {
+            hoogste = waarde
+        }
+    }
+    basic.showString("Hoogste: " + hoogste)
 })
 let totaal = 0
 let list: number[] = []
+let hoogste = 0
+hoogste = 0
 let lijst: number[] = []
 for (let index = 0; index < 10; index++) {
     list.push(randint(1, 100))
